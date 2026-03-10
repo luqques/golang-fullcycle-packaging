bd4ea972031e05afd00ff318cc09b19238aa4f70
Lucas da Cunha <luques.cunha@gmail.com>
2026-03-09 22:12:49 -0300

    criação do go mod e exportando struct

---
+++ cmd/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/luques/golang-fullcycle-packaging/math"
+
+func main() {
+	m := math.Math{A: 5, B: 10}
+	println(m.A, m.B)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/luques/golang-fullcycle-packaging
+
+go 1.25.4
